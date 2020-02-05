8f857eaac029c468201299b4a5408f80a45696e9
Alexey Lushnikov <al.lushnikov@yandex.ru>
2020-02-05 16:21:00 +0300

    fix

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 export default function About() {
   return (
     <Layout>
-      <p>This is the about page NEW</p>
+      <p>This is the about page NEW 2</p>
     </Layout>
   );
 }
